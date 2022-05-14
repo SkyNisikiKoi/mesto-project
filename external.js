686f8934ebf6e7d30d8a75937757c2aadeac6aae
Kate Sidoriuk <CCEBEPA@yandex.ru>
2022-05-14 11:20:35 +0200

    Project for click version

---
+++ external.js
@@ -0,0 +1,9 @@
+function showForm() {
+    let shadow = document.getElementsByClassName("shadow_show")[0];
+    shadow.style.display = "flex";
+  }
+
+  function hideForm() {
+    let shadow = document.getElementsByClassName("shadow_show")[0];
+    shadow.style.display = "none";
+  }
